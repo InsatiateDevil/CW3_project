daa92a01138f1eb0e386532d07cdab68031947ac
ILIDANUM <ILIDANUM@yandex.ru>
2024-03-03 16:16:28 +0300

    Big edit code

--- tests/test_load_json.py
+++ tests/test_load_json.py
@@ -1,8 +1,10 @@
+import os
+from config import ROOT_DIR
 from utils.load_json import load_json
 
 
 def test_load_json():
-    assert load_json("/home/ilidanum/CW3_project/CW3/tests/operations_for_tests.json")[:1] == [
+    assert load_json(os.path.join(ROOT_DIR, 'tests', 'operations_for_tests.json'))[:1] == [
         {
             "id": 667307132,
             "state": "EXECUTED",
